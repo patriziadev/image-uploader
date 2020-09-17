b68284680f80ab85602d661bb0f24dd843b5410b
Patrizia Zaio <patrizia@MacBook-Pro-di-Patrizia.local>
2020-09-17 15:27:41 +0200

    test: create the test for app.component

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,11 +1,16 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { By } from '@angular/platform-browser';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         AppComponent
+      ],
+      imports : [
+        HttpClientModule
       ],
     }).compileComponents();
   }));
@@ -22,10 +27,27 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('image-uploader');
   });
 
-  it('should render title', () => {
+  it('should use the progress value from the service', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const uploadService = fixture.debugElement.injector.get(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('image-uploader app is running!');
+    expect(uploadService.isLoading).toEqual(app.isLoading);
+  });
+
+  it('should use the errorMessage from the service', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    const uploadService = fixture.debugElement.injector.get(AppComponent);
+    fixture.detectChanges();
+    expect(uploadService.errorMessage).toEqual(app.errorMessage);
+  });
+
+  it('should not display the error if there is no message', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    app.errorMessage = '';
+    fixture.detectChanges();
+    expect(fixture.debugElement.query(By.css('.error'))).toBeNull();
   });
 });
